c7d5a2da9ddd2c1b74594234ee5447943c43875e
Amanda Munch <amandajmunch@gmail.com>
2020-03-22 13:51:24 -0400

    added components and building out search component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+  render(){
+    return(
+      <div classname="app">
+      Test
+      </div>
+    )
+  }
+}
+
+export default App;
